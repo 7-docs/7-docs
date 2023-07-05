7846261079427412aa62c45cd8a2464df7b30006
Lars Kappert <lars@webpro.nl>
2023-07-05 09:44:37 +0200

    Alternative regex to fix exception in Safari

--- packages/shared/src/string.ts
+++ packages/shared/src/string.ts
@@ -2,8 +2,8 @@ export const ucFirst = (s: string) => s.charAt(0).toUpperCase() + s.slice(1);
 
 export const normalizeNamespace = (value: string) => value.replace(/[./-]/g, '_');
 
-export const splitTextIntoSentences = (content: string) =>
-  content
-    .replace(/\s+/gm, ' ')
-    .trim()
-    .split(/(?<=[.?!]\s)\s*/g);
+export const splitTextIntoSentences = (content: string) => {
+  const trimmedContent = content.replace(/\s+/gm, ' ').trim();
+  const sentences = trimmedContent.match(/[^.?!]+[.?!]+/g); // Safari throws at positive lookbehinds: .split(/(?<=[.?!]\s)\s*/g);
+  return sentences ?? [];
+};
